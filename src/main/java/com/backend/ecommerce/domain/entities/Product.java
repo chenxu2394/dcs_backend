da32b3b9dd74f68f2781b8f41d456f38deb20242
Chen Xu <cxu.self@gmail.com>
2024-08-06 22:25:10 +0300

    feat: Update database connection URL in application.properties

--- src/main/java/com/backend/ecommerce/domain/entities/Product.java
+++ src/main/java/com/backend/ecommerce/domain/entities/Product.java
@@ -1,13 +1,11 @@
 package com.backend.ecommerce.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "product", schema = "ecommerce")
 @Setter
 @Getter
 public class Product {
