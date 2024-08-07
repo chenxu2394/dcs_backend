27b6da548ee9c35688e4d1f10f5c2bb002c09987
Chen Xu <cxu.self@gmail.com>
2024-08-07 10:10:26 +0300

    refactor: UserService with jpa

--- src/main/java/com/backend/ecommerce/application/UserService.java
+++ src/main/java/com/backend/ecommerce/application/UserService.java
@@ -2,34 +2,33 @@ package com.backend.ecommerce.application;
 
 import com.backend.ecommerce.domain.entities.User;
 import com.backend.ecommerce.domain.interfaces.IUserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
 
-    private final IUserRepository userRepository;
-    public UserService(IUserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private IUserRepository userRepository;
 
     public List<User> getAllUsers() {
         return userRepository.getAllUsers();
     }
-
-    public Optional<User> getUserById(int id) {
-        return userRepository.getUserById(id);
-    }
-
-    public boolean createUser(User user) {
-        return userRepository.createUser(user);
-    }
-
-    public boolean updateUser(User user) {
-        return userRepository.updateUser(user);
-    }
-
-    public boolean deleteUser(int id) {
-        return userRepository.deleteUser(id);
-    }
+//
+//    public Optional<User> getUserById(int id) {
+//        return userRepository.getUserById(id);
+//    }
+//
+//    public boolean createUser(User user) {
+//        return userRepository.createUser(user);
+//    }
+//
+//    public boolean updateUser(User user) {
+//        return userRepository.updateUser(user);
+//    }
+//
+//    public boolean deleteUser(int id) {
+//        return userRepository.deleteUser(id);
+//    }
 }
