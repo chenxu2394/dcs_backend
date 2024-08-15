382cbb95c8ed2f6f4b33a89adbb7875c2f6791a0
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-08-15 13:44:36 +0300

    Add Jwt util

---
+++ src/main/java/com/backend/ecommerce/shared/exceptions/security/JwtUtil.java
@@ -0,0 +1,67 @@
+package com.backend.ecommerce.shared.exceptions.security;
+
+import com.backend.ecommerce.domain.entities.User;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+    @Value("${JWT_SECRET}")
+    private String SECRET_KEY;
+
+    public String generateToken(User user){
+
+        return Jwts
+                .builder()
+                .subject(user.getUsername())
+                .claim("user_id", user.getId())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver){
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isValid(String token, UserDetails user){
+        String email = extractUsername(token);
+        return email.equals(user.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiredAt = extractClaim(token, Claims::getExpiration);
+        return expiredAt.before(new Date());
+    }
+}
