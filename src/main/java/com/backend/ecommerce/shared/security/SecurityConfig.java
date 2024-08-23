164b53f20fe96241240b9519fe4b16f4c886f02a
Chen Xu <cxu.self@gmail.com>
2024-08-23 21:35:45 +0300

    Fix: CORS issue

--- src/main/java/com/backend/ecommerce/shared/security/SecurityConfig.java
+++ src/main/java/com/backend/ecommerce/shared/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req -> req.requestMatchers(
