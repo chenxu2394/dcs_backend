62956de6b6d3e5b83b3f3c1d2e2f1423485b1faa
Chen Xu <cxu.self@gmail.com>
2024-09-18 09:45:34 +0300

    fix: Update Dto

--- src/main/java/com/backend/ecommerce/application/dto/product/UpdateProductDto.java
+++ src/main/java/com/backend/ecommerce/application/dto/product/UpdateProductDto.java
@@ -1,14 +1,15 @@
 package com.backend.ecommerce.application.dto.product;
 
 import java.util.UUID;
+import java.util.List;
 
 public record UpdateProductDto(
-        UUID id,
-        String name,
-        String description,
-        int quantity,
-        double price,
-        int discount,
-        UUID categoryId
-) {
+                UUID id,
+                String name,
+                String description,
+                int quantity,
+                double price,
+                int discount,
+                UUID categoryId,
+                List<String> imageUrls) {
 }
